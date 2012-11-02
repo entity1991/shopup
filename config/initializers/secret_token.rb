f265e118919ba5132ad13d82b2cb0645c55b0c45
entity1991 <ruslankuzma@gmail.com>
2012-11-02 17:02:15 +0200

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopUp::Application.config.secret_token = '84b015f833bfb38bca81377046cfa6f5fba5f191131ceee638db84cbd1606a6091b86e48c9947ed88001afb11be97ef5183863b6f8edd2aa4ca0cf7074e3c686'
